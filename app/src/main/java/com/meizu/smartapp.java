7dce72110902147d3008552368a3218b2c87f996
Oleg Filippov <enternetacum@yandex.ru>
2020-07-29 00:28:15 +0300

    Update app

--- app/src/main/java/com/meizu/smartapp.java
+++ app/src/main/java/com/meizu/smartapp.java
@@ -2,19 +2,16 @@ package com.meizu;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.preference.EditTextPreference;
-import androidx.preference.ListPreference;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceGroup;
+import androidx.preference.SwitchPreference;
 
-import java.util.Map;
+import android.provider.Settings;
 
 public class smartapp extends AppCompatActivity {
 
@@ -32,12 +29,57 @@ public class smartapp extends AppCompatActivity {
         }
     }
 
-    public static class SettingsFragment extends PreferenceFragmentCompat {
+    public static class SettingsFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener {
 
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             // Indicate here the XML resource you created above that holds the preferences
             setPreferencesFromResource(R.xml.preferences, rootKey);
+            initSummary(getPreferenceScreen());
+        }
+
+        @Override
+        public void onResume() {
+            super.onResume();
+            // Set up a listener whenever a key changes
+            getPreferenceScreen().getSharedPreferences()
+                    .registerOnSharedPreferenceChangeListener(this);
+        }
+
+        @Override
+        public void onPause() {
+            super.onPause();
+            // Unregister the listener whenever a key changes
+            getPreferenceScreen().getSharedPreferences()
+                    .unregisterOnSharedPreferenceChangeListener(this);
+        }
+
+        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+            updatePrefSummary(findPreference(key));
+        }
+
+        private void initSummary(Preference p) {
+            if (p instanceof PreferenceGroup) {
+                PreferenceGroup pGrp = (PreferenceGroup) p;
+                for (int i = 0; i < pGrp.getPreferenceCount(); i++) {
+                    initSummary(pGrp.getPreference(i));
+                }
+            } else {
+                updatePrefSummary(p);
+            }
+        }
+
+        private void updatePrefSummary(Preference p) {
+            if (p instanceof EditTextPreference) {
+                EditTextPreference editTextPref = (EditTextPreference) p;
+                p.setSummary(editTextPref.getText());
+            }
+            if (p instanceof SwitchPreference) {
+                SwitchPreference switchPref = (SwitchPreference) p;
+
+//                Settings.System.putInt(null, "power_long_press_voice", (int) (switchPref.isChecked() ? 1 : 0));
+                System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa running: " + switchPref.isChecked());
+            }
         }
     }
 }
